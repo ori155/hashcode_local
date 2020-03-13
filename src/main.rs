a8bebc3389a519ed820e4fe11a06e0cf0254a4d5
ori <oriashur155@gmail.com>
2020-03-13 12:58:04 +0200

    add basic tests

--- src/main.rs
+++ src/main.rs
@@ -45,10 +45,8 @@ pub mod teams_db {
 }
 
 use teams_db::TeamsDb;
+use crate::filters::team_registration;
 
-fn with_db(db: TeamsDb) -> impl Filter<Extract = (TeamsDb,), Error = std::convert::Infallible> + Clone {
-    warp::any().map(move || db.clone())
-}
 
 mod handlers {
     use super::{Team, TeamsDb};
@@ -66,28 +64,87 @@ mod handlers {
     pub async fn list_teams(teams_db: TeamsDb) -> Result<impl warp::Reply, std::convert::Infallible> {
         let listed_teams: Vec<String> = teams_db.list_team_names().await;
 
-        Ok(format!("Teams: {:?}", listed_teams))
+        Ok(warp::reply::json(&listed_teams))
+    }
+}
+
+mod filters {
+    use crate::teams_db::TeamsDb;
+    use warp::Filter;
+
+    fn with_db(db: TeamsDb) -> impl Filter<Extract = (TeamsDb,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || db.clone())
+    }
+
+    pub fn team_registration(teams: TeamsDb) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+    {
+        warp::post()
+            .and(warp::path::path("register_team"))
+            .and(warp::body::json())
+            .and(with_db(teams))
+            .and_then( crate::handlers::add_team)
+    }
+
+    pub fn list_teams(teams: TeamsDb) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+    {
+        warp::get()
+            .and(warp::path("teams"))
+            .and(warp::path::end())
+            .and(with_db(teams.clone()))
+            .and_then(crate::handlers::list_teams)
+    }
+
+    pub fn game_api(teams: TeamsDb) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+    {
+        team_registration(teams.clone())
+            .or(list_teams(teams.clone()))
     }
 }
 
 #[tokio::main]
 async fn main() {
+    use filters::game_api;
 
     let teams = TeamsDb::new();
 
-    let team_registration = warp::post()
-        .and(warp::body::json())
-        .and(with_db(teams.clone()))
-        .and_then( handlers::add_team);
+    warp::serve(game_api(teams)).run(([127, 0, 0, 1], 8080)).await;
 
-    let list_teams = warp::get()
-        .and(warp::path("teams"))
-        .and(warp::path::end())
-        .and(with_db(teams.clone()))
-        .and_then(handlers::list_teams);
+}
 
-    let routes = team_registration.or(list_teams);
+#[cfg(test)]
+mod tests {
+    use crate::teams_db::TeamsDb;
+    use crate::Team;
 
-    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+    #[tokio::test]
+    async fn test_list_empty_teams() {
+        let teams_db = TeamsDb::new();
+        let api = crate::filters::game_api(teams_db.clone());
+
+        let res = warp::test::request()
+            .path("/teams")
+            .reply(&api).await;
+
+        assert_eq!(res.status(), http::StatusCode::OK);
+        assert_eq!(res.body(), "[]");
+    }
+
+    #[tokio::test]
+    async fn test_add_team() {
+        let teams_db = TeamsDb::new();
+        let api = crate::filters::game_api(teams_db.clone());
+
+        let new_team = Team{ name: "first_team".to_owned(), participants: vec!["ori".to_owned()] };
+
+        let res = warp::test::request()
+            .path("/register_team")
+            .method("POST")
+            .json(&new_team)
+            .reply(&api).await;
+
+        assert_eq!(res.status(), http::StatusCode::OK);
+
+        assert!(teams_db.contains(&new_team).await);
+    }
 
 }
